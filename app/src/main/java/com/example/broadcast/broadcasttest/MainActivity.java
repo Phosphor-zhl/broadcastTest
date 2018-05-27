75161f768288f28bac8e7e6f88e790b7ac5f666a
Phosphor-zhl <837163669@qq.com>
2018-05-27 14:45:03 +0800

    修改

--- app/src/main/java/com/example/broadcast/broadcasttest/MainActivity.java
+++ app/src/main/java/com/example/broadcast/broadcasttest/MainActivity.java
@@ -1,50 +1,43 @@
 package com.example.broadcast.broadcasttest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+
 import android.os.Bundle;
 import android.content.IntentFilter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import android.widget.Toast;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
 
+public class MainActivity extends Activity  {
 
-public class MainActivity extends AppCompatActivity {
-
-    private String action="com.android.server.scannerservice.broadcast";
+    private String action="com.example.chinaautoid";
     private MyReceiver receiver;
 
-    Button send;
     TextView txtReceive;
+    Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //获取程序界面中的按钮
         send = (Button)findViewById(R.id.btnSend);
         txtReceive = (TextView)findViewById(R.id.txtReceive);
-
+        //initBroadcastReciever();
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 long time=System.currentTimeMillis();
-
-                //创建Intent
                 Intent intent = new Intent();
                 intent.setAction(action);
-                intent.putExtra("msg", "简单的消息" + time);
-                //发送广播
+                intent.putExtra("scannerdata", "Sample data:" + time);
                 sendBroadcast(intent);
             }
         });
-
     }
 
     @Override
@@ -52,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         receiver=new MyReceiver();
         IntentFilter filter=new IntentFilter(action);
-        registerReceiver(receiver, filter);
+        registerReceiver(receiver,filter);
     }
 
     @Override
@@ -60,16 +53,21 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         unregisterReceiver(receiver);
     }
+
+    /**
+     * 初始化广播接收器，AUTOID系列安卓产品上的系统软件扫描工具相对应
+     */
+    private void initBroadcastReciever() {
+        Intent intent = new Intent();
+        intent.putExtra("barcode_send_mode", "BROADCAST");
+        intent.putExtra("endchar", "NONE");
+        sendBroadcast(intent);
+    }
+
     private class MyReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            String  barcode = intent.getStringExtra("scannerdata");
-            //barcode 扫描到的条码数据
-            txtReceive.setText(intent.getStringExtra("msg"));
-
-            Toast.makeText(context,
-                    "接收到的Intent的Action为：" + intent.getAction() + "\n 消息内容是：" + intent.getStringExtra("msg"),
-                    Toast.LENGTH_LONG).show();
+            txtReceive.setText(intent.getStringExtra("scannerdata") );
         }
     }
 }
